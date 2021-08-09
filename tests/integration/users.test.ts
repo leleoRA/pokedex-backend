4832383bd7413da272654f8962ca24063746f0a5
jotabraga <jpmbraga.feg@gmail.com>
2021-08-08 21:14:40 -0300

    Sign up tests done

--- tests/integration/users.test.ts
+++ tests/integration/users.test.ts
@@ -1,5 +1,6 @@
 import supertest from "supertest";
 import { getConnection } from "typeorm";
+import * as userFactory from "../factories/userFactory";
 
 import app, { init } from "../../src/app";
 import { createUser } from "../factories/userFactory";
@@ -17,20 +18,32 @@ afterAll(async () => {
   await getConnection().close();
 });
 
-describe("GET /users", () => {
-  it("should answer with text \"OK!\" and status 200", async () => {
-    const user = await createUser();
+describe("POST /sign-up", () => {
 
-    const response = await supertest(app).get("/users");
-    
-    expect(response.body).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          email: user.email
-        })
-      ])
-    );
-
-    expect(response.status).toBe(200);
+  it("should answer with status 201 for a valid input data", async () => {
+    const user = userFactory.createUser();
+    const response = await supertest(app).post("/sign-up").send(user);
+    expect(response.status).toBe(201);    
   });
+
+  it("should answer with status 400 for password and confirm password divergence", async () => {
+    const user = {email:"teste@teste.com",password:"rightOne",confirmPassword:"wrongOne"}
+    const response = await supertest(app).post("/sign-up").send(user);
+    expect(response.status).toBe(400);    
+  });
+
+  it("should answer with status 400 for a invalid input data", async () => {
+    const user = {email:"12345",password:"rightOne",confirmPassword:"rightOne"}
+    const response = await supertest(app).post("/sign-up").send(user);
+    expect(response.status).toBe(400);    
+  });
+
+  it("should answer with status 409 if the user already exist", async () => {
+    const user = userFactory.createUser;
+    await supertest(app).post("/sign-up").send(user)
+    const response = await supertest(app).post("/sign-up").send(user);
+    expect(response.status).toBe(409);    
+  });
+
+
 });
